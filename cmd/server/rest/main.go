14b6937d437aa3ffc25eb13cce6942ca8cc9c045
Antony Ho <ntonyworkshop@gmail.com>
2020-08-31 01:58:21 +0200

    Add some TODO

--- cmd/server/rest/main.go
+++ cmd/server/rest/main.go
@@ -4,8 +4,9 @@ import "flag"
 
 var (
 	dbHost = flag.String("db", "localhost", "Database connection string")
-	prod   = flag.Bool("prod", true, "Flag for production instance")
-	test   = flag.Bool("test", false, "Flag for testing instance")
+	// TODO - database credential
+	prod = flag.Bool("prod", true, "Flag for production instance")
+	test = flag.Bool("test", false, "Flag for testing instance")
 )
 
 func main() {
